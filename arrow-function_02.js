8562a3f223a1c24da3b2932bf881547a32dca47a
Jhonatan Henrique Favoreto <jhonatan.favoreto18@gmail.com>
2026-02-09 10:53:11 -0300

    feat: Cria e finaliza parte 2 de revisão js

---
+++ arrow-function_02.js
@@ -0,0 +1,20 @@
+// 5. Converter Celsius para Fahrenheit
+function converter(celsius) {
+	return (celsius * 9/5) + 32;
+}
+
+// 6. Multiplicar por 10
+const multiDez = n => n * 10;
+
+// 7. Calcular Idade em Dias
+const idadeEmDias = anos => anos * 365;
+
+// 8. Converter para Maiúsculo
+const gritar = str => str.toUpperCase();
+
+// 9. Desconto de 5%
+const aplicarDesconto = valor => valor * 0.05;
+
+// 10. Subtração Simples
+const subtrair = (a, b) => a - b;
+
